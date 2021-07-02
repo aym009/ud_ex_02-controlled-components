04125197bee8eb757fd5d8cab80a8e16781419ee
aym009 <aym.ine.012009@gmail.com>
2021-07-02 16:01:37 +0000

    Add solution

---
+++ src/DeleteLastItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const DeleteLastItem = props => {
+  const handleDeleteLastItem = event => {
+    props.onDeleteLastItem()
+  }
+  
+  return(
+    <button onClick={handleDeleteLastItem} disabled={props.buttonDisabled}>
+      Delete Last Item
+    </button>
+  )
+}
+
+DeleteLastItem.PropTypes = {
+  onDeleteLastItem: PropTypes.func.isRequired,
+  buttonDisabled: PropTypes.bool.isRequired
+}
+
+export default DeleteLastItem;
